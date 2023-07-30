b8384a4f490db08f943fafef71d3c877cfa16a2e
Anbarasan6628 <anbarasananbu408@gmail.com>
2023-07-30 04:50:41 +0000

    First Commit

---
+++ dotnetapp/Migrations/20230729105722_Five.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetapp.Migrations
+{
+    public partial class Five : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastRejectedDate",
+                table: "LoanApplicants",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastRejectedDate",
+                table: "LoanApplicants");
+        }
+    }
+}
---
+++ dotnetapp/Migrations/20230729162006_Eight.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetapp.Migrations
+{
+    public partial class Eight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppliedDateTime",
+                table: "LoanApplicants");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentFileName",
+                table: "LoanApplicants");
+
+            migrationBuilder.DropColumn(
+                name: "LastRejectedDate",
+                table: "LoanApplicants");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AppliedDateTime",
+                table: "LoanApplicants",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentFileName",
+                table: "LoanApplicants",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastRejectedDate",
+                table: "LoanApplicants",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
